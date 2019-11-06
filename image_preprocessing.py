19879085e1ff303b2964d4e07c35d4094d236802
nkacoroski <nkacoroski@gmail.com>
2019-11-06 09:43:22 -0800

    Continue exploring different exif tools.

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -2,24 +2,47 @@
 
 import datetime
 from exif import Image
+import os
+import re
 
 def change_datetimes(image_path, time_delta):
     """Take in image file and time change as a time delta object.
     Change exif datetime, datetime_digitized, and datetime_original
     Save file with updated exif datetimes."""
-    # open image
+    # Open image.
     with open(image_path, 'rb') as image_file:
         image = Image(image_file)
-    # generate new datetime
+    # Generate new datetime.
     old_datetime = datetime.datetime.strptime(image.datetime, '%Y:%m:%d %H:%M:%S')
     new_datetime  = (old_datetime + time_delta).strftime("%Y:%m:%d %H:%M:%S")
-    # replace all exif datetimes with new datetime
+    # Replace all exif datetimes with new datetime.
     image.datetime = new_datetime
     image.datetime_digitized = new_datetime
     image.datetime_original = new_datetime
-    # Rewrite image file
+    # Rewrite image file.
     with open(image_path, 'wb') as new_image_file:
         new_image_file.write(image.get_file()) 
+    print(f"{image_path[7::]} datetime fixed!")
+    
+
+def rename_image(image_path):
+    """Take in image path and rename image by site,
+    camera, and datetime."""
+    # Get image datetime.
+    with open(image_path, 'rb') as image_file:
+        image = Image(image_file)
+    datetime = image.datetime
+    dt = re.sub(r"\s+", "", datetime)
+    dt = re.sub(r"\:+", "", dt)
+    camera = ""
+    if image_path.startswith('IMG'):
+        camera += 'c2'
+    else:
+        camera += 'c1'
+    name = "./data/s1" + camera + "_" + dt + ".jpg"
+    os.rename(image_path, name)
+    print(f"{image_path[7::]} renamed!")
+
 
 # TO-DO: get pyexif dump working and extract temperature data,
 # consider tessaract python OCR instead?
